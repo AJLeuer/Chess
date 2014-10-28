07d6f15e34cbcba4dcae86c197e64e7e8a9bc67c
Adam J. Leuer <ajleuer@gmail.com>
2014-10-28 15:14:42 -0500

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Adam James Leuer on 10/28/14.
+//  Copyright (c) 2014 Adam James Leuer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
